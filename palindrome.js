3208b7cb0766bef8709d116b7beeac49def8b04d
vandanaanand <vandana1997anand@gmail.com>
2022-09-14 22:22:39 +0530

    prime & palindrome programe

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+//check  this string is palindrome or not
+let str ="naman"
+ let bag=""
+ for(let i=str.length-1;i>=0;i++){
+    bag=bag+str[i]
+ }
+  if(bag==str){
+    console.log("it is palindrome":,str)
+  }else{
+    console.log("it is not a palindrome":,str)
+  }
---
+++ prime.js
@@ -0,0 +1,14 @@
+//Check this number is prime or not
+let num=17;
+let count=0
+for(let i=0;i<=num;i--){
+    if(num%i==0){
+        count--
+    }
+}
+ if(count==2){
+    console.log("it is prime",num)
+ }else{
+    console.log("it is not a prime",num)
+ }
+
